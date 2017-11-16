766d9ad30eb73a11212c7ccc4527ac1c152a89e3
Dennis Wronka <dennis.wronka@schauinsland-reisen.de>
2017-11-16 16:28:35 +0100

    Adjusted default user-agent to better match that used in HTTPConnection Adjusted default user-agent to better match that used in HTTPConnection

--- src/EMail.php
+++ src/EMail.php
@@ -12,7 +12,7 @@ namespace unrealization\PHPClassCollection;
  * @subpackage EMail
  * @link http://php-classes.sourceforge.net/ PHP Class Collection
  * @author Dennis Wronka <reptiler@users.sourceforge.net>
- * @version 2.0.0
+ * @version 2.0.1
  * @license http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html LGPL 2.1
  */
 class EMail
@@ -91,7 +91,7 @@ class EMail
 	 * @param string $notificationTo
 	 * @param string $userAgent
 	 */
-	public function __construct(string $from, string $subject, string $replyTo = '', string $notificationTo = '', string $userAgent = 'PHP/reptiler/eMail')
+	public function __construct(string $from, string $subject, string $replyTo = '', string $notificationTo = '', string $userAgent = 'PHP/unrealization/eMail')
 	{
 		if (preg_match('@<.*>@', $from) > 0)
 		{
